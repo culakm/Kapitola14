feb9eaa997f17f511b5f20214291f0c10f08402c
culakm <culak.martin@gmail.com>
2015-02-19 00:01:01 +0100

    automaticky (z strings.xml) sa generuju mena aktivit v hlavnej aktivite

--- app/src/main/java/com/hellbilling/kapitola14/MainActivity.java
+++ app/src/main/java/com/hellbilling/kapitola14/MainActivity.java
@@ -7,42 +7,59 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 
 public class MainActivity extends ListActivity {
     private TextView selection;
-    private String[] items={
-                "pako item date - time"
-    };
-
+    private Field[] fields;
+    private ArrayList<String>  activitiesList = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_main);
 
-        // tu su mena aktivit dynamicky nacitane, ale aj tak je to blbe, treba to dorobit, aj selection ktory furt este berie nazvy z items
-        //http://stackoverflow.com/questions/14000844/get-list-of-the-strings-present-in-strings-xml-file-in-android
-        ArrayList<String>  pako = new ArrayList<String>();
-        pako.add(getString(R.string.title_activity_activity1) + "magor");
-        Toast.makeText(getBaseContext(), getString(R.string.title_activity_activity1), Toast.LENGTH_LONG).show();
+        // Najdi hodnoty vsetky title_activity_activity a uloz do pola activitiesList
+        fields = R.string.class.getFields();
 
-        setListAdapter(new ArrayAdapter<String>(this,
+        //for (int  i =0; i < fields.length; i++) {
+        for(Field field : fields)  {
+            int resId = getResources().getIdentifier(field.getName(), "string", getPackageName());
+            String fieldName = field.getName();
+            String fieldValue = "";
+            if (resId != 0) {
+                fieldValue = getResources().getString(resId);
+            }
+            if (fieldName.contains("title_activity_activity")) {
+                activitiesList.add(fieldValue);
+            }
+        }
+       // Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
+
+        // Automaticky podla vsetkych stringov co obsahuju title_activity_activity v strings.xml
+        // vytvor vybery vsetkych aktivity
+        //
+        // este by bolo dobre zistit ako automaticky spustat aktivity v onListItemClick
+        setListAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
-                pako));
-        selection=(TextView)findViewById(R.id.selection);
+                activitiesList));
 
+        selection=(TextView)findViewById(R.id.selection);
     }
 
     public void onListItemClick(ListView parent, View v, int position, long id) {
-        selection.setText(items[position]);
+        selection.setText(activitiesList.get(position));
         Intent intent;
         switch(position) {
             case 0:
                 intent = new Intent(this, Activity1.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(this, activity2.class); //toto bohuzial zacina malym pismenom
                 startActivity(intent);
                 break;
         }
