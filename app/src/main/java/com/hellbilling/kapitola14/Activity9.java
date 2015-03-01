b3c4f3fb924471243e8ccd53a655a4260522086e
culakm <culak.martin@gmail.com>
2015-03-01 19:49:48 +0100

    posielanie go funguje

--- app/src/main/java/com/hellbilling/kapitola14/Activity9.java
+++ app/src/main/java/com/hellbilling/kapitola14/Activity9.java
@@ -1,11 +1,10 @@
 package com.hellbilling.kapitola14;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.EditText;
 
 
 public class Activity9 extends Activity {
@@ -21,11 +20,18 @@ public class Activity9 extends Activity {
         // v AndroidManifest.xml musi byt zadane pre povolenie internetu
         //<uses-permission android:name="android.permission.INTERNET"/>
         // povolenie javascriptu
-        //browser.getSettings().setJavaScriptEnabled(true);
+        browser.getSettings().setJavaScriptEnabled(true);
         // getSettings vracia instanciu triedy WebSettings ktora specifikuje nastavenia browseru
         //browser.loadUrl("http://google.com");
 
         // Obsah zo stringu
-        browser.loadData("<html><body><b>pako</b></body></html>","text/html","UTF-8");
+        browser.loadData("<html><body><b>Tuto sa zobrazi</b></body></html>","text/html","UTF-8");
+    }
+
+    public void changeURL(View view) {
+        EditText Url = (EditText) findViewById(R.id.myUrl);
+        String message = Url.getText().toString();
+        // getSettings vracia instanciu triedy WebSettings ktora specifikuje nastavenia browseru
+        browser.loadUrl("http://"+message);
     }
 }
